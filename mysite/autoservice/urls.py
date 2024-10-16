6d879460c19c947255686ab0b64d04e6c39caa61
TautvydasKre <144778291+TautvydasKre@users.noreply.github.com>
2024-10-16 08:29:24 +0300

    addedd auth to page

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path("", views.index, name="index"),
     path("cars/", views.cars, name="cars"),
     path("cars/<int:car_id>", views.car, name="car"),
     path("orders/", views.OrderListView.as_view(), name="orders"),
     path("orders/<int:pk>", views.OrderDetailView.as_view(), name="order"),
+    path("search/", views.search, name="search"),
 ]
--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator
 from .models import Service, Order, Car
+from django.db.models import Q
 
 # Create your views here.
 
@@ -10,18 +11,21 @@ def index(request):
     num_orders = Order.objects.filter(status="i").count()
     num_cars = Car.objects.count()
 
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         "num_services": num_services,
         "num_orders": num_orders,
         "num_cars": num_cars,
+        "num_visits": num_visits,
     }
     return render(request, template_name="index.html", context=context)
 
 
 def cars(request):
     cars = Car.objects.all()
-    paginator = Paginator(cars, per_page=2)
+    paginator = Paginator(cars, per_page=6)
     page_number = request.GET.get("page")
     paged_cars = paginator.get_page(page_number)
     return render(request, template_name="cars.html", context={"cars": paged_cars})
@@ -39,3 +43,12 @@ class OrderDetailView(DetailView):
     model = Order
     template_name = "order.html"
     context_object_name = "order"
+
+def search(request):
+    query = request.GET.get("query")
+    cars_search_results = Car.objects.filter(Q(client_name__icontains=query) | Q(car_model__make__icontains=query) | Q(car_model__model__icontains=query) | Q(plate__icontains=query) | Q(vin_code__icontains=query))
+    context = {
+        "query": query,
+        "cars": cars_search_results,
+    }
+    return render(request, template_name="search.html", context=context)
